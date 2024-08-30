17b5e062b22211519fe931dd5afbe10f501a7db5
unknown <arcady.oboukhov@gmail.com>
2024-08-30 13:09:36 -0400

    inital commit

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_VideoexcesV3fixed_FILE C:/Users/User/Documents/VideoexcesV3fixed/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/VideoexcesV3fixed.exe)
+set(__QT_DEPLOY_TARGET_VideoexcesV3fixed_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_VideoexcesV3fixed_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
---
+++ old save becuase i fucked up git/made detection for checkbox/settings.h
@@ -0,0 +1,39 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <QWidget>
+#include <QSlider>
+#include <QLabel>
+#include <QPushButton>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include "C://Users//User//Documents//VideoexcesV3fixed//json-develop//json-develop//single_include//nlohmann//json.hpp"
+#include "mainwindow.h" // Include the MainWindow header
+
+namespace Ui {
+class Settings;
+}
+
+class MainWindow; // Forward declaration
+
+class Settings : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit Settings(MainWindow* mainWindow, QWidget *parent = nullptr);
+    ~Settings();
+
+    bool isMp3CheckBoxChecked() const;
+
+    void loadSettingsFromJson(); // Declaration
+
+public slots:
+    void onGoBackButtonClicked();
+
+public:
+    Ui::Settings *ui; // Pointer to the UI class
+    MainWindow *mainWindow;
+};
+
+#endif // SETTINGS_H
